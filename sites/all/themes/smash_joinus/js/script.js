198b4f9d2500c0b884885fcc1d08004947fe595d
Geoffrey Roberts <g.roberts@blackicemedia.com>
2012-08-23 01:29:34 +1000

    Added theme

---
+++ sites/all/themes/smash_joinus/js/script.js
@@ -0,0 +1,25 @@
+/**
+ * @file
+ * A JavaScript file for the theme.
+ *
+ * In order for this JavaScript to be loaded on pages, see the instructions in
+ * the README.txt next to this file.
+ */
+
+// JavaScript should be made compatible with libraries other than jQuery by
+// wrapping it with an "anonymous closure". See:
+// - http://drupal.org/node/1446420
+// - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+(function ($, Drupal, window, document, undefined) {
+
+$(document).ready(function(){
+
+// Place your code here.
+
+  $('.pane-departments, .pane-positions').delegate('.view-content a', 'mouseup', function(e){
+    $(this).addClass('active');
+  });
+
+});
+
+})(jQuery, Drupal, this, this.document);
